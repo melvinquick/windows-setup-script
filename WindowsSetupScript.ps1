de78ad312739ac15f7dce211c73baca8c3b68eac
Charlie Quick <cquickreal00@gmail.com>
2022-09-08 14:48:26 -0400

    Update WindowsSetupScript.ps1
    Cleaned up Winget section and put in acknowledgement to Chris Titus Tech.

--- WindowsSetupScript.ps1
+++ WindowsSetupScript.ps1
@@ -75,61 +75,50 @@ if ($userInput.ToLower() -eq "y") {
     Write-Host "WINGET" -ForegroundColor Green
     Write-Host "=============================================" -ForegroundColor Green
 
-    # Check if Winget is installed already
-    Write-Host "`nChecking to see if Winget is installed."
-    $isInstalled = $false
+    # Check if Winget is installed already and install it if not (I took this section from Chris Titus Tech's WinUtil located here: https://github.com/ChrisTitusTech/winutil/blob/main/winutil.ps1), so shoutout to him!
 
-    if (winget -v) {
-        $isInstalled = $true
+    if (((((Get-ComputerInfo).OSName.IndexOf("LTSC")) -ne -1) -or ((Get-ComputerInfo).OSName.IndexOf("Server") -ne -1)) -and (((Get-ComputerInfo).WindowsVersion) -ge "1809")) {
+        #Checks if Windows edition is LTSC/Server 2019+
+        #Manually Installing Winget
+        Write-Host "Running Alternative Installer for LTSC/Server Editions"
+
+        #Download Needed Files
+        Write-Host "Downloading Needed Files..."
+        Start-BitsTransfer -Source "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -Destination "./Microsoft.VCLibs.x64.14.00.Desktop.appx"
+        Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Destination "./Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/b0a0692da1034339b76dce1c298a1e42_License1.xml" -Destination "./b0a0692da1034339b76dce1c298a1e42_License1.xml"
+
+        #Installing Packages
+        Write-Host "Installing Packages..."
+        Add-AppxProvisionedPackage -Online -PackagePath ".\Microsoft.VCLibs.x64.14.00.Desktop.appx" -SkipLicense
+        Add-AppxProvisionedPackage -Online -PackagePath ".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -LicensePath ".\b0a0692da1034339b76dce1c298a1e42_License1.xml"
+        Write-Host "winget Installed (Reboot might be required before winget will work)"
+
+        #Sleep for 5 seconds to maximize chance that winget will work without reboot
+        Write-Host "Pausing for 5 seconds to maximize chance that winget will work without reboot"
+        Start-Sleep -s 5
+
+        #Removing no longer needed Files
+        Write-Host "Removing no longer needed Files..."
+        Remove-Item -Path ".\Microsoft.VCLibs.x64.14.00.Desktop.appx" -Force
+        Remove-Item -Path ".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force
+        Remove-Item -Path ".\b0a0692da1034339b76dce1c298a1e42_License1.xml" -Force
+        Write-Host "Removed Files that are no longer needed"
     }
-
-    if ($isInstalled -eq $false) {
-        Write-Host "Winget was not installed. Downloading and installing now."
-
-        if (((((Get-ComputerInfo).OSName.IndexOf("LTSC")) -ne -1) -or ((Get-ComputerInfo).OSName.IndexOf("Server") -ne -1)) -and (((Get-ComputerInfo).WindowsVersion) -ge "1809")) {
-            #Checks if Windows edition is LTSC/Server 2019+
-            #Manually Installing Winget
-            Write-Host "Running Alternative Installer for LTSC/Server Editions"
-
-            #Download Needed Files
-            Write-Host "Downloading Needed Files..."
-            Start-BitsTransfer -Source "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -Destination "./Microsoft.VCLibs.x64.14.00.Desktop.appx"
-            Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Destination "./Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-            Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/b0a0692da1034339b76dce1c298a1e42_License1.xml" -Destination "./b0a0692da1034339b76dce1c298a1e42_License1.xml"
-
-            #Installing Packages
-            Write-Host "Installing Packages..."
-            Add-AppxProvisionedPackage -Online -PackagePath ".\Microsoft.VCLibs.x64.14.00.Desktop.appx" -SkipLicense
-            Add-AppxProvisionedPackage -Online -PackagePath ".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -LicensePath ".\b0a0692da1034339b76dce1c298a1e42_License1.xml"
-            Write-Host "winget Installed (Reboot might be required before winget will work)"
-
-            #Sleep for 5 seconds to maximize chance that winget will work without reboot
-            Write-Host "Pausing for 5 seconds to maximize chance that winget will work without reboot"
-            Start-Sleep -s 5
-
-            #Removing no longer needed Files
-            Write-Host "Removing no longer needed Files..."
-            Remove-Item -Path ".\Microsoft.VCLibs.x64.14.00.Desktop.appx" -Force
-            Remove-Item -Path ".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force
-            Remove-Item -Path ".\b0a0692da1034339b76dce1c298a1e42_License1.xml" -Force
-            Write-Host "Removed Files that are no longer needed"
-        }
-        elseif (((Get-ComputerInfo).WindowsVersion) -lt "1809") {
-            #Checks if Windows Version is too old for winget
-            Write-Host "Winget is not supported on this version of Windows (Pre-1809)"
-        }
-        else {
-            #Installing Winget from the Microsoft Store
-            Write-Host "Winget not found, installing it now."
-            Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
-            $nid = (Get-Process AppInstaller).Id
-            Wait-Process -Id $nid
-            Start-Sleep -s 5
-            Write-Host "Winget Installed"
-        }
-        Write-Host "Winget is now installed."
-        refreshenv
+    elseif (((Get-ComputerInfo).WindowsVersion) -lt "1809") {
+        #Checks if Windows Version is too old for winget
+        Write-Host "Winget is not supported on this version of Windows (Pre-1809)"
     }
+    else {
+        #Installing Winget from the Microsoft Store
+        Write-Host "Winget not found, installing it now."
+        Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+        $nid = (Get-Process AppInstaller).Id
+        Wait-Process -Id $nid
+        Start-Sleep -s 5
+        Write-Host "Winget Installed"
+    }
+    refreshenv
 
     # Install programs
     Write-Host "Now installing programs via Winget."
