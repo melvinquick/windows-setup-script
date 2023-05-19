637a4186c8865266afa8f240a172f6e861fbbcd9
Charlie Quick <cquickreal00@gmail.com>
2023-05-19 10:27:53 -0400

    Update windows-setup-script.ps1
    Scoop App Installs Removed
    -------------------------------
    Poetry
    
    Winget App Installs Added
    -----------------------------
    Git (Needed for Scoop section)

--- windows-setup-script.ps1
+++ windows-setup-script.ps1
@@ -6,9 +6,9 @@ $conf_url = "https://github.com/cquick00/windows-config-files.git"
 # App Install Lists
 $ps_modules = @("PSWindowsUpdate", "PSWordCloud")
 $scoop_apps_base = @("main/7zip", "main/git")
-$scoop_apps_main = @("extras/alacritty", "extras/android-messages", "extras/ipscan", "extras/autohotkey", "extras/bleachbit", "extras/blender", "extras/caesium-image-compressor", "extras/discord", "extras/eartrumpet", "games/epic-games-launcher", "nerd-fonts/FiraCode-NF", "extras/gimp", "extras/github", "extras/inkscape", "extras/inno-setup", "extras/kdenlive", "extras/makemkv", "games/mgba", "extras/firefox", "extras/mremoteng", "main/nano", "main/neofetch", "main/nodejs", "extras/obs-studio",  "main/poetry", "main/python", "extras/qbittorrent-enhanced", "extras/revouninstaller", "main/scrcpy", "extras/screentogif", "main/starship", "versions/steam", "extras/sysinternals", "extras/telegram", "extras/ventoy", "extras/vlc", "extras/vortex", "extras/vscode", "extras/winscp", "extras/wireshark", "extras/zoom")
+$scoop_apps_main = @("extras/alacritty", "extras/android-messages", "extras/ipscan", "extras/autohotkey", "extras/bleachbit", "extras/blender", "extras/caesium-image-compressor", "extras/discord", "extras/eartrumpet", "games/epic-games-launcher", "nerd-fonts/FiraCode-NF", "extras/gimp", "extras/github", "extras/inkscape", "extras/inno-setup", "extras/kdenlive", "extras/makemkv", "games/mgba", "extras/firefox", "extras/mremoteng", "main/nano", "main/neofetch", "main/nodejs", "extras/obs-studio", "main/python", "extras/qbittorrent-enhanced", "extras/revouninstaller", "main/scrcpy", "extras/screentogif", "main/starship", "versions/steam", "extras/sysinternals", "extras/telegram", "extras/ventoy", "extras/vlc", "extras/vortex", "extras/vscode", "extras/winscp", "extras/wireshark", "extras/zoom")
 $scoop_buckets = @("extras", "games", "nerd-fonts", "versions")
-$winget_apps = @("Elgato.4KCaptureUtility", "Epilogue.EpilogueOperator", "Google.Drive", "Microsoft.DirectX", "Microsoft.VCRedist.2015+.x64", "Microsoft.VCRedist.2015+.x86", "Microsoft.PowerShell")
+$winget_apps = @("Elgato.4KCaptureUtility", "Epilogue.EpilogueOperator", "Git.Git", "Google.Drive", "Microsoft.DirectX", "Microsoft.VCRedist.2015+.x64", "Microsoft.VCRedist.2015+.x86", "Microsoft.PowerShell")
 
 # System/User Directories
 $conf_dir = "$HOME\Downloads\windows-config-files\home"
